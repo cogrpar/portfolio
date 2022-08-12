72896026f09cf43d168a61359d1996bce7c49142
Owen Welsh <34008762+cogrpar@users.noreply.github.com>
2022-08-12 23:39:49 +0000

    added text and navigation

--- main.js
+++ main.js
@@ -22,11 +22,12 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const cameraDistance = 6; // distance from the camera to the origin on x z plane
-const theta = Math.PI/2; // angle of camera in standard position in the x z plane
+const cameraDistance = 7; // distance from the camera to the origin on x z plane
+let theta = 0; // angle of camera in standard position in the x z plane
 camera.position.x = Math.cos(theta)*cameraDistance;
 camera.position.z = Math.sin(theta)*cameraDistance;
-camera.position.y = 1;
+camera.position.y = 1.2;
+camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
@@ -34,15 +35,14 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 renderer.render(scene, camera);
 
 
 
 // TEMP orbit controls
-const controls = new THREE.OrbitControls(camera, renderer.domElement);
+/*const controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 0, 0);
-controls.update();
+controls.update();*/
 
 
 
@@ -57,9 +57,9 @@ scene.add(ambientLight);
 
 
 // TEMP helpers
-/*const lightHelper = new THREE.PointLightHelper(pointLight)
+/*const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper)*/
+scene.add(lightHelper, gridHelper);*/
 
 
 
@@ -95,6 +95,12 @@ loader.load('models/rings.glb', function(gltf) {
 });
 
 // creating text
+const primaryLinks = ['About Me', 'Projects', 'My Work'];
+let numPrimaryLinks = primaryLinks.length;
+let primaryLinksTheta = Math.PI/(1.5*numPrimaryLinks);
+let primaryLinksDict = {}; // dictionary storing the 3d objects of the primary links
+const primaryLinksGroup = new THREE.Group(); // create group of objects for the primary links
+
 const ttfLoader = new THREE.TTFLoader();
 const fontLoader = new THREE.FontLoader();
 ttfLoader.load(
@@ -105,47 +111,56 @@ ttfLoader.load(
     let font = fontLoader.parse(loaded);
     // do something with the font
 		console.log(font);
-    const text = new THREE.TextGeometry('PLACEHOLDER',{
-      font: font,
-      size: 30,
-      height: 3,
-      curveSegments: 12,
-      bevelEnabled: false
-    });
-    const textMesh = new THREE.Mesh(
-      text,
-      new THREE.MeshPhysicalMaterial({  
-        roughness: 0.1,  
-        transmission: 0.7, // Add transparency
-        thickness: 0.1,
-        color: 0xf03030,
-      })
-    );
-    textMesh.geometry.center();
-    textMesh.scale.x = 0.01; 
-    textMesh.scale.y = 0.01; 
-    textMesh.scale.z = 0.01;
 
-    textMesh.position.z = 3.5;
-    textMesh.position.y = 0.5;
-    textMesh.rotation.x = -Math.PI/15;
-    scene.add(textMesh)
+    // for each link, load it in
+    for (let i = 0; i < numPrimaryLinks; i++){
+      let text = new THREE.TextGeometry(primaryLinks[i],{
+        font: font,
+        size: 0.3,
+        height: 0.03,
+        curveSegments: 12,
+        bevelEnabled: false
+      });
+      primaryLinksDict[primaryLinks[i]] = new THREE.Mesh(
+        text,
+        new THREE.MeshPhysicalMaterial({  
+          roughness: 0.1,  
+          transmission: 0.7, // Add transparency
+          thickness: 0.1,
+          color: 0xf03030,
+        })
+      );
+      // position each link around the planet
+      primaryLinksDict[primaryLinks[i]].geometry.center();
+      primaryLinksDict[primaryLinks[i]].position.x = Math.cos(primaryLinksTheta*i)*3.5;
+      primaryLinksDict[primaryLinks[i]].position.z = -Math.sin(primaryLinksTheta*i)*3.5;
+      primaryLinksDict[primaryLinks[i]].position.y = 0.5;
+      
+      primaryLinksDict[primaryLinks[i]].rotation.y = (Math.PI/2)+(primaryLinksTheta*i);
+      primaryLinksGroup.add(primaryLinksDict[primaryLinks[i]]);
+    }
+    scene.add(primaryLinksGroup);
   }
 );
+
 
 
 // animation
 let directions = [];
 let currentDirection = 0;
+let cameraPointX = 0;
+let cameraPointY = 0;
+let cameraPointZ = 0;
+let baseTheta = theta;
 function cameraOscillate(numDirections, severity){ // function that animates the camera oscillating between facing some number of randomly generated directions
   if (directions.length === 0){ // if this is the first time being run, generate the random directions
     for (let i = 0; i < numDirections; i++){
-      directions.push(addArray([camera.rotation.x, camera.rotation.y, camera.rotation.z], getRandomArbitrary(-severity, severity, 3)));
+      directions.push(getRandomArbitrary(-severity, severity, 3));
     }
   }
 
   // calculate how close the camera is to the current direction
-  let del = Math.abs(camera.rotation.x - directions[currentDirection][0]) + Math.abs((camera.rotation.y - directions[currentDirection][1])) + Math.abs((camera.rotation.z - directions[currentDirection][2]));
+  let del = Math.abs(cameraPointX - directions[currentDirection][0]) + Math.abs((cameraPointY - directions[currentDirection][1])) + Math.abs((cameraPointZ - directions[currentDirection][2]));
 
   if (del < 0.1){ // if close enough begin going to the next direction
     if (currentDirection === directions.length-1){
@@ -156,13 +171,52 @@ function cameraOscillate(numDirections, severity){ // function that animates the
     }
   }
   else{ // otherwise go to the current direction
-    let scale = 5000*Math.abs(camera.rotation.x - directions[currentDirection][0]);
-    camera.rotation.x -= (camera.rotation.x - directions[currentDirection][0])/scale;
-    camera.rotation.y -= (camera.rotation.y - directions[currentDirection][1])/scale;
-    camera.rotation.z -= (camera.rotation.z - directions[currentDirection][2])/scale;
+    let scale = 0.01;
+    cameraPointX -= scale * (cameraPointX - directions[currentDirection][0]);
+    cameraPointY -= scale * (cameraPointY - directions[currentDirection][1]);
+    cameraPointZ -= scale * (cameraPointZ - directions[currentDirection][2]);
+    camera.lookAt(cameraPointX, cameraPointY, cameraPointZ);
+  }
+
+  // rotate about the origin if basePos is updated
+  if (Math.abs(theta-baseTheta) > 0.01){
+    if (theta > baseTheta){
+      theta -= 0.01;
+      camera.position.x = Math.cos(theta)*cameraDistance;
+      camera.position.z = Math.sin(theta)*cameraDistance;
+    }
+    else{
+      theta += 0.01;
+      camera.position.x = Math.cos(theta)*cameraDistance;
+      camera.position.z = Math.sin(theta)*cameraDistance;
+    }
   }
 }
 
+// listen for mousedown for navigating the menu
+
+// events to handle mouse presses
+var mouseDown = 0;
+document.body.onmousedown = function(event) { 
+  let x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1; // mouse pos -1 to 1
+  if (x > 0.5 ){
+    // update camera oscillation vars to make camera move to the right
+    directions = [];
+    currentDirection = 0;
+    baseTheta = baseTheta-primaryLinksTheta;
+  }
+  if (x < -0.5){
+    // update camera oscillation vars to make camera move to the left
+    directions = [];
+    currentDirection = 0;
+    baseTheta = baseTheta+primaryLinksTheta;
+  }
+}
+document.body.onmouseup = function() {
+    mouseDown = 0;
+}
+
+// main animation function
 function animate() {
   // rotate the planet and rings
   if (loadPlanet){
@@ -173,7 +227,7 @@ function animate() {
   }
 
   // oscillate the camera
-  cameraOscillate(5, 0.1);
+  cameraOscillate(5, 0.8);
   
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
